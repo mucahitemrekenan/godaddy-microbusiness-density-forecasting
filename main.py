35b7f0cf5a272e0d0ca4df1e026f6c6e1ab706db
mucahitemrekenan <mekenan.ds@gmail.com>
2023-02-15 21:22:53 +0300

    Version 1.2 I filtered data by county and fitted the model in a loop.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LinearRegression
+import lightgbm
 from lightgbm import LGBMRegressor
 import matplotlib.pyplot as plt
 import matplotlib
+from tqdm import tqdm
 import warnings
 import math
 warnings.filterwarnings('module')
@@ -30,19 +33,28 @@ test['month'] = test['date'].dt.month
 
 test.drop(columns=test.columns.difference(['cfips', 'year', 'month']), inplace=True)
 
+preds_test = pd.DataFrame(index=test.index, columns=['preds'])
+preds_train = pd.DataFrame(index=x_train.index, columns=['preds'])
+
 # mm_scaler = MinMaxScaler((0,1))
 # x_train['cfips'] = mm_scaler.fit_transform(x_train[['cfips']])
 # test['cfips'] = mm_scaler.transform(test[['cfips']])
 
-lgb = LGBMRegressor(n_estimators=10000, max_depth=-1)
-lgb.fit(x_train, y_train, categorical_feature=['cfips'])
-predictions = lgb.predict(test)
-predictions_train = lgb.predict(x_train)
+# model = LGBMRegressor(n_estimators=2000, max_depth=4, num_leaves=8)
+for county in tqdm(x_train['cfips'].unique()):
+    model = LinearRegression()
+
+    index_train = x_train.loc[x_train['cfips'] == county].index
+    index_test = test.loc[test['cfips'] == county].index
+
+    model.fit(x_train.loc[index_train], y_train[index_train])
+    preds_test.loc[index_test, 'preds'] = model.predict(test.loc[index_test])
+    preds_train.loc[index_train, 'preds'] = model.predict(x_train.loc[index_train])
 
 plt.plot(pd.Series(y_train), label='actual')
-plt.plot(pd.Series(predictions_train), label='prediction')
+plt.plot(pd.Series(preds_train['preds']), label='prediction')
 plt.legend()
 plt.show()
 
-sample_sub['microbusiness_density'] = pd.Series(predictions)
-sample_sub.to_csv('sample_submission_1_01.csv', index=False)
+sample_sub['microbusiness_density'] = pd.Series(preds_test['preds'])
+sample_sub.to_csv('submissions/sample_submission_1_2.csv', index=False)
